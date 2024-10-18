8363592341b7232680c2d9c7c45312bd946a7b45
Yon <ejliud@gmail.com>
2024-10-18 17:21:08 +0300

    feat: Add data to models
    - Add Model data to `cso`, `guest`, `log`

--- server/models/cso.go
+++ server/models/cso.go
@@ -1,1 +1,11 @@
 package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type CSO struct {
+	gorm.Model
+	UserName       string `json:"username"`
+	HashedPassword string `json:"hashedPassword"`
+}
--- server/models/guest.go
+++ server/models/guest.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Guest struct {
+	gorm.Model
+	Name             string `json:"name"`
+	ID_Number        string `json:"id"`
+	VehiclePlate     string `json:"vehicle_plate"`
+	Affiliation      string `json:"affiliation"`
+	Inviter          string `json:"inviter"`
+	Purpose          string `json:"purpose"`
+	SecurityResp     string `json:"security"`
+	EntryApproved    string `json:"entry"`
+	SecurityClear    string `json:"securityClear"`
+	SecurityClearExp string `json:"securityClearExp"`
+	SecurityMgrAppr  string `json:"securityMgrAppr"`
+	ApprovalExp      string `json:"approvalExp"`
+	Notes            string `json:"notes"`
+}

67eebfe841887b5d68b595566f92f44fca709e06
Yon <ejliud@gmail.com>
2024-10-18 17:31:16 +0300

    feat: Add db Initialization and migration
    - Add DB variable
    - Add InitDB function for database initalization

--- server/database/database.go
+++ server/database/database.go
@@ -1,1 +1,25 @@
 package database
+
+import (
+	"log"
+
+	"rami/models"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("rami.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	DB.AutoMigrate(&models.Guest{}, &models.Log{}, &models.CSO{})
+	if err != nil {
+		log.Fatal("Failed to migrate database: ", err)
+	}
+}

755d02dfb3a7029e37e8ec409cd0675b4a1b3981
Yon <ejliud@gmail.com>
2024-11-06 13:48:12 +0200

    Test: Add test utils
    - Added a few functions to help with tests

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,7 @@ module rami
 go 1.22.5
 
 require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
---
+++ server/service/test_utils.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"math/rand"
+	"rami/database"
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+func InitiateTestDB() *gorm.DB {
+	db := database.InitDB(":memory:")
+
+	db.Exec("DELETE FROM visitors;")
+	db.Exec("DELETE FROM logs;")
+	db.Exec("DELETE FROM csos;")
+
+	return db
+}
+
+func generateRandomTimestamp() int {
+	return int(time.Now().Unix()) + rand.Intn(1000000)
+}
+
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var result strings.Builder
+	for i := 0; i < length; i++ {
+		result.WriteByte(charset[rand.Intn(len(charset))])
+	}
+	return result.String()
+}
+
+func RandomChoice(options []string) string {
+	rand.Seed(time.Now().UnixNano())        // Seed the random number generator
+	return options[rand.Intn(len(options))] // Select a random index
+}

e2205b9b7f1c23150d8d0b8ea6c37a5f65b0582b
Yon <ejliud@gmail.com>
2024-11-08 14:44:27 +0200

    refactor: Add not null flags to visitor's model

--- server/models/visitor.go
+++ server/models/visitor.go
@@ -6,19 +6,19 @@ import (
 
 type Visitor struct {
 	gorm.Model
-	Name                    string `json:"name"`
-	CredentialsNumber       string `json:"credentialsNumber"`       // credentials number
-	CredentialType          string `json:"credentialType"`          // passport, id, hoger
-	VehiclePlate            string `json:"vehiclePlate"`            // vehicle plate if any
-	Association             string `json:"association"`             // company / organization
-	Inviter                 string `json:"inviter"`                 // person who invited the Visitor
-	Purpose                 string `json:"purpose"`                 // purpose of visit
-	EntryApproval           bool   `json:"entryApproval"`           // entry approval
-	EntryExpriry            int    `json:"entryExpriry"`            // entry expiry UNIX
-	SecurityResponse        string `json:"securityResponse"`        // what type of security measures needed upon entry
-	ClearanceLevel          string `json:"clearanceLevel"`          // security clearance level
-	ClearanceExpiry         int    `json:"clearanceExpiry"`         // security clearance expiry UNIX
-	SecurityOfficerApproval bool   `json:"securityOfficerApproval"` // security officer approval in MOD
-	Notes                   string `json:"notes"`
-	IsInside                bool   `json:"isInside"` // is the visitor inside the premises
+	Name                    string `json:"name" gorm:"not null"`                    // name of the visitor
+	CredentialsNumber       string `json:"credentialsNumber" gorm:"not null"`       // credentials number
+	CredentialType          string `json:"credentialType" gorm:"not null"`          // passport, id, hoger
+	VehiclePlate            string `json:"vehiclePlate" gorm:"not null"`            // vehicle plate if any
+	Association             string `json:"association" gorm:"not null"`             // company / organization
+	Inviter                 string `json:"inviter" gorm:"not null"`                 // person who invited the Visitor
+	Purpose                 string `json:"purpose" gorm:"not null"`                 // purpose of visit
+	EntryApproval           bool   `json:"entryApproval" gorm:"not null"`           // entry approval
+	EntryExpriry            int    `json:"entryExpriry" gorm:"not null"`            // entry expiry UNIX
+	SecurityResponse        string `json:"securityResponse" gorm:"not null"`        // what type of security measures needed upon entry
+	ClearanceLevel          string `json:"clearanceLevel" gorm:"not null"`          // security clearance level
+	ClearanceExpiry         int    `json:"clearanceExpiry" gorm:"not null"`         // security clearance expiry UNIX
+	SecurityOfficerApproval bool   `json:"securityOfficerApproval" gorm:"not null"` // security officer approval in MOD
+	Notes                   string `json:"notes" gorm:"not null"`
+	IsInside                bool   `json:"isInside" gorm:"not null"` // is the visitor inside the premises
 }

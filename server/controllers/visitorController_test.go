60e172313eb9787195e3287082fc73224432add6
Yon <ejliud@gmail.com>
2024-11-09 18:03:30 +0200

    test: Upload Get visitor tests

--- server/controllers/visitorController_test.go
+++ server/controllers/visitorController_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"rami/models"
 	"rami/services"
 	"rami/utils"
 	"testing"
@@ -89,3 +90,83 @@ func TestCreateExistingVisitor(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
 	}
 }
+
+func TestGetAllVisitorsHandler(t *testing.T) {
+	initVisitorControllerTest()
+
+	visitors := make([]models.Visitor, 5)
+	for i := 0; i < 5; i++ {
+		visitor := utils.GenerateRandomVisitor()
+		visitorService.CreateVisitor(&visitor)
+		visitors[i] = visitor
+	}
+
+	req, err := http.NewRequest("GET", "/visitors", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	visitorRouter.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	var retrievedVisitors []models.Visitor
+	if err := json.NewDecoder(rr.Body).Decode(&retrievedVisitors); err != nil {
+		t.Fatalf("Error decoding response: %v", err)
+	}
+
+	if len(retrievedVisitors) != 5 {
+		t.Errorf("Expected 5 visitors, got %d", len(retrievedVisitors))
+	}
+
+	for i := 0; i < 5; i++ {
+		if visitors[i] != retrievedVisitors[i] {
+			t.Errorf("Expected visitor %v, got %v", visitors[i], retrievedVisitors[i])
+		}
+	}
+}
+
+func TestGetVisitorByCredentialsNumberHandler(t *testing.T) {
+	initVisitorControllerTest()
+
+	visitor := utils.GenerateRandomVisitor()
+	visitorService.CreateVisitor(&visitor)
+
+	req, err := http.NewRequest("GET", "/visitors/"+visitor.CredentialsNumber, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	visitorRouter.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	var retrievedVisitor models.Visitor
+	if err := json.NewDecoder(rr.Body).Decode(&retrievedVisitor); err != nil {
+		t.Fatalf("Error decoding response: %v", err)
+	}
+
+	if visitor != retrievedVisitor {
+		t.Errorf("Expected visitor %v, got %v", visitor, retrievedVisitor)
+	}
+}
+
+func TestNonExistantGetVisitorByCredentialsNumberHandler(t *testing.T) {
+	initVisitorControllerTest()
+
+	req, err := http.NewRequest("GET", "/visitors/123", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	visitorRouter.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
+	}
+}
+

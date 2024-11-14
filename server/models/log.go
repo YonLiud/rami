95184ea028a39141af98b6af851104e6cff90ad5
Yon <ejliud@gmail.com>
2024-11-14 12:30:46 +0200

    refactor: ungormify
    - Remove gorm as requested by client

--- server/models/log.go
+++ server/models/log.go
@@ -1,18 +1,7 @@
 package models
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
-func GetCurrentTimestamp() int {
-	return int(time.Now().Unix())
-}
-
 type Log struct {
-	gorm.Model
-	Event     string `json:"event"`
-	Serial    string `json:"serial"` // username / personal number associated with the event's subject
-	Timestamp int    `json:"timestamp"`
+	Event     string
+	Serial    string
+	Timestamp string
 }
--- server/models/visitor.go
+++ server/models/visitor.go
@@ -1,24 +1,19 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
-
 type Visitor struct {
-	gorm.Model
-	Name                    string `json:"name" gorm:"not null"`                                        // name of the visitor
-	CredentialsNumber       string `json:"credentialsNumber" gorm:"not null;uniqueIndex;default:null" ` // credentials number
-	CredentialType          string `json:"credentialType" gorm:"not null"`                              // passport, id, hoger
-	VehiclePlate            string `json:"vehiclePlate" gorm:"not null"`                                // vehicle plate if any
-	Association             string `json:"association" gorm:"not null"`                                 // company / organization
-	Inviter                 string `json:"inviter" gorm:"not null"`                                     // person who invited the Visitor
-	Purpose                 string `json:"purpose" gorm:"not null"`                                     // purpose of visit
-	EntryApproval           bool   `json:"entryApproval" gorm:"not null"`                               // entry approval
-	EntryExpriry            int    `json:"entryExpriry" gorm:"not null"`                                // entry expiry UNIX
-	SecurityResponse        string `json:"securityResponse" gorm:"not null"`                            // what type of security measures needed upon entry
-	ClearanceLevel          string `json:"clearanceLevel" gorm:"not null"`                              // security clearance level
-	ClearanceExpiry         int    `json:"clearanceExpiry" gorm:"not null"`                             // security clearance expiry UNIX
-	SecurityOfficerApproval bool   `json:"securityOfficerApproval" gorm:"not null"`                     // security officer approval in MOD
-	Notes                   string `json:"notes" gorm:"not null"`
-	Inside                  bool   `json:"inside" gorm:"not null; default:false"`
+	Name                    string
+	CredentialsNumber       string
+	CredentialType          string
+	VehiclePlate            string
+	Association             string
+	Inviter                 string
+	Purpose                 string
+	EntryApproval           bool
+	EntryExpiry             int
+	SecurityResponse        string
+	ClearanceLevel          string
+	ClearanceExpiry         int
+	SecurityOfficerApproval bool
+	Notes                   string
+	Inside                  bool
 }
